baedfe80a6ad31934d32505ced5cd7e16f97a67a
denvolkov <denvolkov@inbox.ru>
2021-01-18 13:36:31 +0300

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by test on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
