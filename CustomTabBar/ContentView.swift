04054ada7ad449a7fc36b63fc5156d3c73b899b6
denvolkov <denvolkov@inbox.ru>
2021-01-18 15:08:21 +0300

    added CurvedShape

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                         Image(systemName: "house")
                             .font(.largeTitle)
                     }.foregroundColor(self.selected == 0 ? .black : .gray)
-                    Spacer(minLength: 12)
+                    Spacer()
                     
                     Button(action: {
                         self.selected = 1
@@ -30,7 +30,8 @@ struct ContentView: View {
                         Image (systemName: "magnifyingglass")
                             .font(.largeTitle)
                     }.foregroundColor(self.selected == 1 ? .black : .gray)
-                    Spacer().frame(width: 120)
+                    Spacer()
+                        .frame(width: 120)
                     
                     Button(action: {
                         self.selected = 2
@@ -38,7 +39,7 @@ struct ContentView: View {
                         Image(systemName: "person")
                             .font(.largeTitle)
                     }.foregroundColor(self.selected == 2 ? .black : .gray)
-                    Spacer(minLength: 12)
+                    Spacer()
                     
                     Button(action: {
                         self.selected = 3
@@ -46,19 +47,45 @@ struct ContentView: View {
                         Image(systemName: "menubar.dock.rectangle")
                             .font(.largeTitle)
                     }.foregroundColor(self.selected == 3 ? .black : .gray)
-
+                    
                     
                 }
                 .padding()
-                .padding(.horizontal, 22)
-                .background(Color.green)
+                .padding(.horizontal, 15)
+                .background(CurvedShape())
+                
+                Button(action: {
+                    //
+                }) {
+                    Image(systemName: "heart")
+                        .font(.largeTitle)
+                        .padding(15)
+                } .background(Color.yellow)
+                .clipShape(Circle())
+                .offset(y: -20)
+                .shadow(radius: 5)
             }
-        }
+            
+        }.background(Color.green.edgesIgnoringSafeArea(.all))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CurvedShape: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x:0, y:0))
+            path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 0))
+            path.addLine(to: CGPoint(x: UIScreen.main.bounds.width, y: 55))
+            
+            path.addArc(center: CGPoint(x: UIScreen.main.bounds.width / 2, y: 55), radius: 40, startAngle: .zero, endAngle: .init(degrees: 180), clockwise: true)
+            path.addLine(to: CGPoint(x: 0, y: 55))
+        } .fill(Color.white)
+        .rotationEffect(.init(degrees: 180))
     }
 }
