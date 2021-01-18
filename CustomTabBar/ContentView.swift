7a5a4c0045940811a0d20b36f9c3fbb621466255
denvolkov <denvolkov@inbox.ru>
2021-01-18 14:35:07 +0300

    Added SFsymbol

--- CustomTabBar/ContentView.swift
+++ CustomTabBar/ContentView.swift
@@ -2,15 +2,58 @@
 //  ContentView.swift
 //  CustomTabBar
 //
-//  Created by test on 18.01.2021.
+//  Created by Denis Volkov on 18.01.2021.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selected = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            
+            ZStack {
+                
+                HStack {
+                    Button(action: {
+                        self.selected = 0
+                    }) {
+                        Image(systemName: "house")
+                            .font(.largeTitle)
+                    }.foregroundColor(self.selected == 0 ? .black : .gray)
+                    Spacer(minLength: 12)
+                    
+                    Button(action: {
+                        self.selected = 1
+                    }) {
+                        Image (systemName: "magnifyingglass")
+                            .font(.largeTitle)
+                    }.foregroundColor(self.selected == 1 ? .black : .gray)
+                    Spacer().frame(width: 120)
+                    
+                    Button(action: {
+                        self.selected = 2
+                    }) {
+                        Image(systemName: "person")
+                            .font(.largeTitle)
+                    }.foregroundColor(self.selected == 2 ? .black : .gray)
+                    Spacer(minLength: 12)
+                    
+                    Button(action: {
+                        self.selected = 3
+                    }) {
+                        Image(systemName: "menubar.dock.rectangle")
+                            .font(.largeTitle)
+                    }.foregroundColor(self.selected == 3 ? .black : .gray)
+
+                    
+                }
+                .padding()
+                .padding(.horizontal, 22)
+                .background(Color.green)
+            }
+        }
     }
 }
 
